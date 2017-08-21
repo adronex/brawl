32def7426f95c33063835e99cdee8e2f5a5c707d
Pavel Sinitskiy <adronex303@gmail.com>
2017-08-21 23:56:45 +0300

    Docker on Windows.

--- server/wait-database.sh
+++ server/wait-database.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 echo "Waiting for mysql"
-while ! mysqladmin ping -h"$MYSQL_HOST" -P"$MYSQL_PORT" --silent; do
-    printf "."
-    sleep 3
-done
-
+while ! mysqladmin ping -h"$MYSQL_HOST" -P"$MYSQL_PORT" --silent;
+do printf "." && sleep 3 ; done > /dev/null
 echo -e "\nmysql ready"
