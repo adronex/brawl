a57cbb923a4a4dc200d2456b321a9ae87c2f23e2
pavel.sinitski <pavel.sinitski@belprog.com>
2017-10-17 15:01:38 +0300

    Auth - little better.

---
+++ server/src/main/java/by/brawl/config/OptionsRequestsFilter.kt
@@ -0,0 +1,32 @@
+package by.brawl.config
+
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import javax.servlet.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class OptionsRequestsFilter: Filter {
+
+    override fun doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) {
+        val response = res as HttpServletResponse
+        val request = req as HttpServletRequest
+        response.setHeader("Access-Control-Allow-Origin", "*")
+        response.setHeader("Access-Control-Allow-Methods", "OPTIONS, PUT, DELETE")
+        response.setHeader("Access-Control-Max-Age", "3600")
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+        if ("OPTIONS".equals(request.method, ignoreCase = true)) {
+            response.status = HttpServletResponse.SC_OK
+        } else {
+            chain.doFilter(req, res)
+        }
+    }
+
+    override fun init(filterConfig: FilterConfig) {}
+
+    override fun destroy() {}
+}
--- server/src/main/resources/static/vue/src/components/AuthBlock.vue
+++ server/src/main/resources/static/vue/src/components/AuthBlock.vue
@@ -1,10 +1,14 @@
 <template>
-    <div class="auth-block">
+    <div class="auth-block" v-if="!getLogin()">
         <form>
             <input type="text" placeholder="Login" ref="login"/>
             <input type="password" placeholder="Password" ref="password"/>
             <input type="button" value="submit" v-on:click="auth()"/>
         </form>
+    </div>
+    <div v-else>
+        <p>Login as {{getLogin()}}</p>
+        <input type="button" value="logout" v-on:click="logout()"/>
     </div>
 </template>
 
@@ -17,17 +21,30 @@
             auth() {
                 const login = this.$refs.login.value;
                 const password = this.$refs.password.value;
-                let requestString = `grant_type=password&login=${login}&password=${password}`;
-                this.$http.headers.common['Authorization'] = btoa('Lola:Bola');
-                this.$http.post(`${config.serverURL}api/login`, requestString)
+                const requestString = `grant_type=password&username=${login}&password=${password}`;
+                this.$http.post(`${config.serverURL}/api/login`, requestString, {
+                    headers: {
+                        'Authorization': `Basic ${btoa('Lola:Bola')}`,
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }
+                })
                     .then(
                         function (response) {
-                            console.log(response);
+                            localStorage.setItem("login", login);
+                            response.body.claimed_in = new Date().getTime();
+                            localStorage.setItem("auth_data", JSON.stringify(response.body));
                         },
                         function (response) {
-                            console.error(response);
+                            alert(`Error: ${response.body.error}, description: ${response.body.error_description}`);
                         }
                     );
+            },
+            logout() {
+                localStorage.removeItem("login");
+                localStorage.removeItem("auth_data");
+            },
+            getLogin() {
+                return localStorage.getItem("login")
             }
         }
     }
