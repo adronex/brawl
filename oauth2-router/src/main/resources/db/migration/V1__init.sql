5aab6fba2efbcf0458cf7c33ff0d1a8db5ec0d52
pavel.sinitski <pavel.sinitski@belprog.com>
2017-10-20 16:57:57 +0300

    Modules refactoring.

--- oauth2-router/src/main/resources/db/migration/V1__init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `account` (
-  `id` VARCHAR(36) NOT NULL,
-  `email` VARCHAR(100) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC));
--- oauth2-router/wait-database.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Waiting for mysql"
-while ! mysqladmin ping -h"$MYSQL_HOST" -P"$MYSQL_PORT" --silent; do
-    printf "."
-    sleep 3
-done
-
-echo -e "\nmysql ready"
